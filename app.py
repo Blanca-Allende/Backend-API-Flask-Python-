892d215dda885c5218172ecbceac7edf68f63eef
Blanca <blancaallende648@gmail.com>
2025-08-17 08:46:38 -0600

    app

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -13,6 +14,7 @@ def phi4():
     return jsonify({"message": "Hola desde phi4!"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
